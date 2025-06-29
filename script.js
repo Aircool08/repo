76e434a14931433387d165e6f1df1f36d742b766
Aircool08 <aircool08@gmx.net>
2025-06-29 19:00:12 +0200

    ds

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const ua = navigator.userAgent.toLowerCase();
 const isAltStoreOrESign = ua.includes("altstore") || ua.includes("esign");
 
 if (isAltStoreOrESign) {
-  // Redirect to JSON if accessed by AltStore/ESign
   window.location.href = "manifest.json";
 }
